8618a809cc3cfdb01d1821d69e7834ef5d6df481
robertokoba7 <okobarobert7@gmail.com>
2023-03-20 17:30:25 +0300

    eslint was disabled

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    /* eslist-disabled*/
+    /* eslint-disabled*/
     const task = true;
     const task2 = false;
   }
