092fee173db52f0474f6c6e2d86f1e21ce0822eb
robertokoba7 <okobarobert7@gmail.com>
2023-03-21 00:28:52 +0300

    [add]: ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,7 +4,7 @@ export default function getFullBudgetObject(income, gdp, capita){
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
     getIncomeInEuros(income) {
