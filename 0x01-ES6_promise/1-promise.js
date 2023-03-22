830fe544cb26ce56aadd81e4598e41c631e81b6f
robertokoba7 <okobarobert7@gmail.com>
2023-03-23 00:45:25 +0300

    return a promise but remember the parameter is a boolean

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,14 @@
-export default function getFullResponseFromAPI() {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (true) {
+    if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
 }
+
+const responsePromise = getFullResponseFromAPI();
+responsePromise
+  .then((response) => console.log('Promise', response))
+  .catch((error) => console.log('Promise', error));
