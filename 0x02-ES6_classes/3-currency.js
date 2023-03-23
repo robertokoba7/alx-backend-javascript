18bea4f87fa85966d2bcc0efc13ee17876ed7c04
robertokoba7 <okobarobert7@gmail.com>
2023-03-23 21:58:19 +0300

    [add]: Class:static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') this._code = code;
+  }
+  
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
