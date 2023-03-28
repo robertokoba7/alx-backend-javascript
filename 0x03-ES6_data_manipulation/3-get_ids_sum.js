2fbea8517be592831e4c7891b691c087b1f6df49
robertokoba7 <okobarobert7@gmail.com>
2023-03-28 18:21:30 +0300

    [add] usage: .reduce functionon the array

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,10 @@
 function getStudentIdsSum(students) {
   if (!Array.isArray(students)) {
-    return 0
+    return 0;
   }
 
   const sumIds = students.reduce((sum, student) => sum + student.id, 0);
   return sumIds;
-};
+}
 
 export default getStudentIdsSum;
