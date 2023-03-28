196979ace9c548dc6a774fe5791420d1ecfa9033
robertokoba7 <okobarobert7@gmail.com>
2023-03-28 18:14:13 +0300

    [add]: Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,10 +5,10 @@ function createInt8TypedArray(length, position, value) {
     throw Error('Position outside range');
   }
 
-// Create an ArrayBuffer of the given length (in bytes) and assign it to the buffer variable
+  // Create an ArrayBuffer of the given length (in bytes) and assign it to the buffer variable
   const buffer = new ArrayBuffer(length);
 
-// Create an Int8Array of the given length (in bytes) and assign it to the int8View variable
+  // Create an Int8Array of the given length (in bytes) and assign it to the int8View variable
   const int8View = new Int8Array(buffer, 0, length);
   int8View.set([value], position); // Set the value at the given position to the given value
   return new DataView(buffer); // Return a DataView of the given length
