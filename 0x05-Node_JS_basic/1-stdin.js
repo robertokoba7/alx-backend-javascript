7aadf1f3437996c10621e8180ccfecf000a90cb2
robertokoba7 <okobarobert7@gmail.com>
2023-05-16 11:35:46 +0300

    [add]: Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
 	const name = process.stdin.read();
-	if (name) {
-		process.stdout.write(`Your name is: ${name}`);
+	if (name !== null) {
+	  process.stdout.write(`Your name is: ${name}`);
 	}
 });
 
